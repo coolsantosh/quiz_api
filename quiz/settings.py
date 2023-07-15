8b858c7f8bef5b37a5540e623a0a335c8cc570d1
santoshrsharma867 <santosh.rsharma867@gmail.com>
2023-07-15 16:45:07 +0530

    updated

--- quiz/settings.py
+++ quiz/settings.py
@@ -126,7 +126,8 @@ USE_TZ = True
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-import os
-STATIC_URL = 'static/'
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+
+
+STATIC_URL= 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
